37eca05a936366197617a21e23b6efe963ffd3a2
Hannah M <hannahemalm616@gmail.com>
2021-06-25 17:26:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season,holiday|
+    #this will print out Season: 
     puts season.to_s.capitalize + ":"
   holiday.each do |holiday, value|
     #splitting words (New_years) at underscore
